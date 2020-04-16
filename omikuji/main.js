98ab632b239eb143a80bb0c7c6eb9f5e30b55695
mb <mb@mba.local>
2020-04-16 17:39:07 +0900

    git7

--- omikuji/main.js
+++ omikuji/main.js
@@ -6,5 +6,19 @@
   btn.addEventListener('click',()=>{
     const n = Math.floor(Math.random()*3);
     btn.textContent= n;
+
+    switch(n){
+      case 0:
+        btn.textContent ='good'
+        break;
+        
+      case 1:
+        btn.textContent ='ok'
+        break;
+      case 2:
+        btn.textContent ='bad'
+        break;
+
+    }
   });
 }
