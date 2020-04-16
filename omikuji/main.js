fbeb68762f8a8041df3a1e7382b1bb0a51f2e21c
mb <mb@mba.local>
2020-04-16 17:46:49 +0900

    omi8

--- omikuji/main.js
+++ omikuji/main.js
@@ -4,21 +4,22 @@
   const btn = document.getElementById('btn');
 
   btn.addEventListener('click',()=>{
-    const n = Math.floor(Math.random()*3);
-    btn.textContent= n;
+    const results = ['GOOD','SOSO','BAD','very bad'];
+    // const n = Math.floor(Math.random()*results.length);
+    // btn.textContent= results[n];
+    btn.textContent= results[Math.floor(Math.random()*results.length)];
 
-    switch(n){
-      case 0:
-        btn.textContent ='good'
-        break;
+    // switch(n){
+    //   case 0:
+    //     btn.textContent ='good'
+    //     break;
         
-      case 1:
-        btn.textContent ='ok'
-        break;
-      case 2:
-        btn.textContent ='bad'
-        break;
-
-    }
+    //   case 1:
+    //     btn.textContent ='ok'
+    //     break;
+    //   case 2:
+    //     btn.textContent ='bad'
+    //     break;
+    // }
   });
 }
